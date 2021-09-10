13e435232b04ce6c2b95fd631b9b86552222185e
Jonny <jona_hanzen@hotmail.com>
2021-09-10 00:54:43 -0300

    projeto spring boot criado com dependencias para lidar com o hibernate

---
+++ src/main/java/br/com/jonnyboy/hibernate/sample/ExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonnyboy.hibernate.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleProjectApplication.class, args);
+	}
+
+}
